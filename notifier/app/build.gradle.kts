92c271593d1ab9cfe468a4ec6bbcf6172be78872
Enzo Santos <enzo.santos29@hotmail.com>
2026-08-21 00:46:57 -0300

    [notifier] chore: add s3 dependency

--- notifier/app/build.gradle.kts
+++ notifier/app/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("com.rabbitmq:amqp-client:5.27.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.0")
     implementation("org.eclipse.angus:angus-mail:2.0.4")
+    implementation("aws.sdk.kotlin:s3:1.5.20")
 }
 
 java {
