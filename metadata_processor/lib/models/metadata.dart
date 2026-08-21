d01c3f132f7aeb4e460fd123ec29afcf7c6f9c39
Enzo Santos <enzo.santos29@hotmail.com>
2026-08-21 08:42:17 -0300

    [metadata_processor] feat: add metadata model

---
+++ metadata_processor/lib/models/metadata.dart
@@ -0,0 +1,17 @@
+class Metadata {
+  final String id;
+  final int width;
+  final int height;
+  final int size;
+  final String mimeType;
+  final String hash;
+
+  const Metadata({
+    required this.id,
+    required this.width,
+    required this.height,
+    required this.size,
+    required this.mimeType,
+    required this.hash,
+  });
+}
