4abb446e90f4aaae712784f618fe2d81401c6a76
Enzo Santos <enzo.santos29@hotmail.com>
2026-08-21 00:19:03 -0300

    [fetcher] feat: add headers to Imgur HTTP request

--- fetcher/src/main.rs
+++ fetcher/src/main.rs
@@ -57,9 +57,26 @@ async fn fetch_image(image_id: &str) -> Result<Option<Vec<u8>>, reqwest::Error> 
         .redirect(reqwest::redirect::Policy::none())
         .build()?;
 
-    let url = format!("https://i.imgur.com/{image_id}.png");
-
-    let response = client.get(url).send().await?;
+    let response = client
+        .get(format!("https://i.imgur.com/{image_id}.png"))
+        .header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+        .header("accept-encoding", "gzip, deflate, br, zstd")
+        .header("accept-language", "pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
+        .header("cache-control", "no-cache")
+        .header("pragma", "no-cache")
+        .header("priority", "u=0, i")
+        .header("referer", "https://i.imgur.com/")
+        .header("upgrade-insecure-requests", "1")
+        .header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36 Edg/147.0.0.0")
+        .header("sec-ch-ua", "\"Microsoft Edge\";v=\"147\", \"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"147\"")
+        .header("sec-ch-ua-mobile", "?0")
+        .header("sec-ch-ua-platform", "\"Windows\"")
+        .header("sec-fetch-dest", "document")
+        .header("sec-fetch-mode", "navigate")
+        .header("sec-fetch-site", "same-site")
+        .header("sec-fetch-user", "?1")
+        .send()
+        .await?;
 
     if response.status() == reqwest::StatusCode::FOUND {
         return Ok(None);
