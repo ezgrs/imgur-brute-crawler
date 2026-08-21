343085dd76d104fa181075bba8c343c6ac58fe83
Enzo Santos <enzo.santos29@hotmail.com>
2026-08-21 00:47:22 -0300

    [notifier] feat: download Image from s3 and send as email attachment

--- notifier/app/src/main/kotlin/org/example/App.kt
+++ notifier/app/src/main/kotlin/org/example/App.kt
@@ -1,16 +1,29 @@
 package org.example
 
+import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
+import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.s3.model.GetObjectRequest
+import aws.smithy.kotlin.runtime.content.toByteArray
+import aws.smithy.kotlin.runtime.net.url.Url
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.rabbitmq.client.ConnectionFactory
+import jakarta.activation.DataHandler
 import jakarta.mail.Authenticator
 import jakarta.mail.Message
 import jakarta.mail.PasswordAuthentication
 import jakarta.mail.Session
 import jakarta.mail.Transport
 import jakarta.mail.internet.InternetAddress
+import jakarta.mail.internet.MimeBodyPart
 import jakarta.mail.internet.MimeMessage
+import jakarta.mail.internet.MimeMultipart
+import jakarta.mail.util.ByteArrayDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import java.util.Properties
 
 data class ImageSavedEvent(
@@ -18,8 +31,9 @@ data class ImageSavedEvent(
 )
 
 fun sendEmail(
-    subject: String,
-    body: String,
+    imageId: String,
+    contentBytes: ByteArray,
+    contentType: String,
 ) {
     val host = System.getenv("SMTP_HOST")
     val port = System.getenv("SMTP_PORT")
@@ -51,14 +65,43 @@ fun sendEmail(
                 Message.RecipientType.TO,
                 InternetAddress.parse(username),
             )
-            setSubject(subject, "UTF-8")
-            setText(body, "UTF-8")
+            setSubject("An Imgur image ($imageId) was just found!", "UTF-8")
+
+            setContent(
+                MimeMultipart("mixed").apply {
+                    val attachment = MimeBodyPart()
+                    attachment.dataHandler =
+                        DataHandler(
+                            ByteArrayDataSource(contentBytes, contentType),
+                        )
+                    attachment.fileName = imageId
+                    attachment.disposition = jakarta.mail.Part.ATTACHMENT
+
+                    addBodyPart(attachment)
+                },
+            )
         }
 
     Transport.send(message)
 }
 
 fun main() {
+    val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    val mapper =
+        jacksonObjectMapper()
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+
+    val s3 =
+        S3Client {
+            region = "us-east-1"
+            endpointUrl = Url.parse("http://minio:9000")
+            credentialsProvider =
+                StaticCredentialsProvider {
+                    accessKeyId = System.getenv("MINIO_ROOT_USERNAME")
+                    secretAccessKey = System.getenv("MINIO_ROOT_PASSWORD")
+                }
+        }
+
     val f =
         ConnectionFactory().apply {
             host = "rabbitmq"
@@ -66,9 +109,6 @@ fun main() {
             username = System.getenv("RABBITMQ_ROOT_USERNAME")
             password = System.getenv("RABBITMQ_ROOT_PASSWORD")
         }
-    val mapper =
-        jacksonObjectMapper()
-            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
 
     f.newConnection().use { c ->
         c.createChannel().use { ch ->
@@ -78,7 +118,21 @@ fun main() {
                 { _, m ->
                     val json = String(m.body)
                     val event: ImageSavedEvent = mapper.readValue(json)
-                    sendEmail("You got mail!", "Image ID: ${event.imageId}")
+
+                    scope.launch {
+                        s3.getObject(
+                            GetObjectRequest {
+                                this.bucket = "images"
+                                this.key = event.imageId
+                            },
+                        ) { response ->
+                            sendEmail(
+                                event.imageId,
+                                response.body?.toByteArray() ?: ByteArray(0),
+                                response.contentType ?: "application/octet-stream",
+                            )
+                        }
+                    }
                 },
                 {},
             )
