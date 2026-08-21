7f9a487e58ecdf364b5c8eb76dff89e54b31241c
Enzo Santos <enzo.santos29@hotmail.com>
2026-08-20 23:21:01 -0300

    [notifier] feat: add support to deserializing RabbitMQ event

--- notifier/app/src/main/kotlin/org/example/App.kt
+++ notifier/app/src/main/kotlin/org/example/App.kt
@@ -1,6 +1,13 @@
 package org.example
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.rabbitmq.client.ConnectionFactory
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+data class ImageSavedEvent(
+    val imageId: String
+)
 
 fun main() {
     val f = ConnectionFactory().apply {
@@ -9,6 +16,8 @@ fun main() {
         username = System.getenv("RABBITMQ_ROOT_USERNAME")
         password = System.getenv("RABBITMQ_ROOT_PASSWORD")
     }
+    val mapper = jacksonObjectMapper()
+        .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
 
     f.newConnection().use { c ->
         c.createChannel().use { ch ->
@@ -16,7 +25,9 @@ fun main() {
                 "image.saved.notifier",
                 true,
                 { _, m ->
-                    println(String(m.body))
+                    val json = String(m.body)
+                    val event: ImageSavedEvent = mapper.readValue(json)
+                    println(event.imageId)
                 },
                 {},
             )
